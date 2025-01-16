e0301f0e1df22c40c2defdef61c6a189e5178885
zu0910 <165825531+zu0910@users.noreply.github.com>
2025-01-15 20:49:08 -0500

    "feat :sparkles: Update ddl.sql"

--- ddl.sql
+++ ddl.sql
@@ -1,70 +1,69 @@
-CREATE DATABASE RepasoMysql2;
--- drop database RepasoMysql2;
-USE RepasoMysql2;
-
+CREATE DATABASE universidad_t2 CHARACTER SET utf8mb4;
+USE universidad_t2;
 
 CREATE TABLE departamento (
-	id_departamento INT(10) PRIMARY KEY,
-    nombre VARCHAR(50)
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL
 );
+
+CREATE TABLE alumno (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    nif VARCHAR(9) UNIQUE,
+    nombre VARCHAR(25) NOT NULL,
+    apellido1 VARCHAR(50) NOT NULL,
+    apellido2 VARCHAR(50),
+    ciudad VARCHAR(25) NOT NULL,
+    direccion VARCHAR(50) NOT NULL,
+    telefono VARCHAR(9),
+    fecha_nacimiento DATE NOT NULL,
+    sexo ENUM('H', 'M') NOT NULL
 );
 
 CREATE TABLE profesor (
-	id_profesor INT(10) PRIMARY KEY,
-	nif VARCHAR(9) NULL,
-	nombre VARCHAR(25),
-	apellido1 VARCHAR(50),
-	apellido2 VARCHAR(50) NULL,
-	ciudad VARCHAR(25),
-	direccion VARCHAR(50),
-	telefono VARCHAR(9) NULL,
-	fecha_nacimiento DATE,
-	sexo ENUM('H','M'),
-    id_departamento INT(10),
-	FOREIGN KEY (id_departamento) REFERENCES departamento(id_departamento)
-
-CREATE TABLE alumno (
-	id_alumno INT(10) PRIMARY KEY,
-	nif VARCHAR(9) NULL,
-	nombre VARCHAR(25),
-	apellido1 VARCHAR(50),
-	apellido2 VARCHAR(50) NULL,
-	ciudad VARCHAR(25),
-	direccion VARCHAR(50),
-	telefono VARCHAR(9) NULL,
-	fecha_nacimiento DATE,
-	sexo ENUM('H','M')
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    nif VARCHAR(9) UNIQUE,
+    nombre VARCHAR(25) NOT NULL,
+    apellido1 VARCHAR(50) NOT NULL,
+    apellido2 VARCHAR(50),
+    ciudad VARCHAR(25) NOT NULL,
+    direccion VARCHAR(50) NOT NULL,
+    telefono VARCHAR(9),
+    fecha_nacimiento DATE NOT NULL,
+    sexo ENUM('H', 'M') NOT NULL,
+    id_departamento INT UNSIGNED NOT NULL,
+    FOREIGN KEY (id_departamento) REFERENCES departamento(id)
 );
-
-CREATE TABLE curso_escolar (
-	id_curso_escolar INT(10) PRIMARY KEY,
-    anyo_inicio YEAR(4),
-    anyo_fin YEAR(4)
+ 
+ CREATE TABLE grado (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL
 );
-
-CREATE TABLE grado (
-	id_grado INT(10) PRIMARY KEY,
-    nombre VARCHAR(100) 
-);
-
+ 
 CREATE TABLE asignatura (
-	id_asignatura INT(10) PRIMARY KEY,
-	nombre VARCHAR(100),
-	creditos FLOAT,
-	tipo ENUM('básica','optativa','obligatoria'),
-	curso TINYINT(3),
-	cuatrimestre TINYINT(3),
-	id_profesor INT(10) NULL,
-	id_grado INT(10),
-	FOREIGN KEY (id_profesor) REFERENCES profesor(id_profesor),
-	FOREIGN KEY(id_grado) REFERENCES grado(id_grado)
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    creditos FLOAT UNSIGNED NOT NULL,
+    tipo ENUM('básica', 'obligatoria', 'optativa') NOT NULL,
+    curso TINYINT UNSIGNED NOT NULL,
+    cuatrimestre TINYINT UNSIGNED NOT NULL,
+    id_profesor INT UNSIGNED,
+    id_grado INT UNSIGNED NOT NULL,
+    FOREIGN KEY(id_profesor) REFERENCES profesor(id),
+    FOREIGN KEY(id_grado) REFERENCES grado(id)
+);
+ 
+CREATE TABLE curso_escolar (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    anyo_inicio YEAR NOT NULL,
+    anyo_fin YEAR NOT NULL
 );
 
-CREATE TABLE alumno_se_matricula_asignatura(
-	id_alumno INT(10),
-    id_asignatura INT(10),
-    id_curso_escolar INT(10),
-    FOREIGN KEY(id_alumno) REFERENCES alumno(id_alumno),
-    FOREIGN KEY(id_asignatura) REFERENCES asignatura(id_asignatura),
-    FOREIGN KEY(id_curso_escolar) REFERENCES curso_escolar(id_curso_escolar)
+CREATE TABLE alumno_se_matricula_asignatura (
+    id_alumno INT UNSIGNED NOT NULL,
+    id_asignatura INT UNSIGNED NOT NULL,
+    id_curso_escolar INT UNSIGNED NOT NULL,
+    PRIMARY KEY (id_alumno, id_asignatura, id_curso_escolar),
+    FOREIGN KEY (id_alumno) REFERENCES alumno(id),
+    FOREIGN KEY (id_asignatura) REFERENCES asignatura(id),
+    FOREIGN KEY (id_curso_escolar) REFERENCES curso_escolar(id)
 );
