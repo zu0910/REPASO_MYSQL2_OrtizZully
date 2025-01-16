d24146b7188dacb0608f073b3b29d911b7f4e88a
zu0910 <zullyortiz.09@gmail.com>
2025-01-16 07:27:06 -0500

    feat :sparkles: readme done

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# REPASO_MYSQL2_OrtizZully
+# REPASO MySQL2 - Universidad T2
+
+## Descripcion
+
+Este proyecto consta de una base de datos donde su funcion es gestionar efectivamente toda la informacion de la Universida T2.
+En la cual incluye eventos, triggers y funciones SQL donde garantizara la integridad de los datos 
+
+El proposito principal de esta base de datos es modelar y administrar la informacion relacionada con:
+
+    - Alumnos, profesores, asignaturas y departamentos.
+    - Matriculas de alumnos en asignaturas asignadas.
+    - Historial academico y auditoria de cambios de datos 
+    - Automatizacion de tareas administrativas mediante eventos y triggers
+
+## Funcionalidad 
+
+1. **Triggers** : Garantiza la integirdad y registrar cambios en la base de datos.
+2. **Eventos** : Realizar tareas automatizadas.
+3. **Funciones**: Calculos de la base de datos.
+4. **Consultas**: Realizar reportes, analisis de la base de datos.
+
+## Requisito del sistema 
+
+Para ejecutar la base de datos se necesita:
+
+    - MySQL Server 
+    - MySQL Workbench 
+    - Visual Studio Code
+
+## Instalacion 
+
+1. Terner MySQL Server y Workbech instalado en tu sistema.
+2. Ejecutar los bases de datos 
+3. Ejecutar el ddl.sql para la creacion de todas las tablas.
+4. Ejecutar el dml.sql para insertar los datos de las tablas creadas.
+5. Prosigue a ejecutar eventos, triggers, consultas y funciones 
+
+## Credutos 
+
+Este Repaso MySQL2 fue elaborado por Zully Fernanda Ortiz Avendaño CC.1092528097 para reforzar y retroalimentar los temas de base de datos MySQL2.
--- dql_eventos.sql
+++ dql_eventos.sql
@@ -1,66 +1,53 @@
 USE RepasoMysql2 ;
 -- eventos 
 
--- 1. ReporteMensualDeAlumnos: Genera un informe mensual con el total de alumnos matriculados por grado y lo 
+-- Evento 
+
+-- -- 1. ReporteMensualDeAlumnos: Genera un informe mensual con el total de alumnos matriculados por grado y lo 
 -- almacena automaticamente 
 
+-- O_O Hay que tener una tabla que almacene el informe mensual del total de alumnos. Se propone la siguiente tabla:
+
+CREATE TABLE informe_mensual_matriculas (
+	id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    grado_id INT UNSIGNED NOT NULL,
+    total_alumnos INT NOT NULL,
+    fecha_informe DATETIME NOT NULL,
+    FOREIGN KEY (grado_id) REFERENCES grado(id)
+);
+
+
 DELIMITER //
-CREATE EVENT ReporteMensual
-ON SCHEDULE EVERY  1 MONTH 
-STARTS '2025-01-31 23:59:59' DO
-BEGIN
-    INSERT INTO ReporteMensualAlumnos (mes, año, id_grado, grado_nombre, total_alumnos)
-    SELECT MONTH(CURRENT_DATE) AS mes, YEAR(CURRENT_DATE) AS año,
-	g.id_grado, g.nombre AS grado_nombre,
-	COUNT(am.id_alumno) AS total_alumnos
-    FROM grado g JOIN asignatura ON g.id_grado = alumno.id_grado
-    JOIN alumno_se_matricula_asignatura am ON alumno.id_asignatura = am.id_asignatura
-    WHERE am.id_alumno IS NOT NULL 
-    GROUP BY g.id_grado, g.nombre;
+CREATE EVENT ReporteMensualDeAlumnos
+ON SCHEDULE EVERY 1 MONTH
+DO 
+BEGIN 
+	INSERT INTO informe_mensual_matriculas
+    (grado_id, total_alumnos, fecha_informe)
+    SELECT id_grado, COUNT(id_alumno), NOW()
+    FROM alumno_se_matricula_asigantura
+    GROUP BY id_grado;
+END ;
+DELIMITER //
+
+-- 2. ActualizarHorasDepartamento: Actualiza el total de horas impartidas por cada departamento al final de cada semestre.
+
+DELIMITER //
+CREATE EVENT ActualizarHorasDepartamento 
+ON SCHEDULE EVERY 6 MONTH 
+DO 
+BEGIN 
+	UPDATE departamento d 
+    JOIN (
+    SELECT p.id_departamento, SUM(a.creditos) AS total_horas FROM asignatura a
+    JOIN profesor p ON p.id = a.id_departamento 
+    GROUP BY p.id_departamento
+    ) AS horas_totales ON d.id = horas_totales.id_departamento 
+    SET d.total_horas_impartidas = horas_totales.total_horas;
 END //
 DELIMITER ;
 
--- 2. ActualizarHorasDepartamento: Actualiza el total de horas impartidas por cada departamento al final de cada semestre.
-delimiter //
-create event ActualizarHoras on schedule every 6 month
-starts '2024-06-30 23:59:59' do
-begin
-    update TotalHoras th
-    join (
-        select profesor.id_departamento, sum(alumno.creditos * 25) as total_horas
-        from asignatura
-        join profesor on alumno.id_profesor = profesor.id_profesor
-        where alumno.cuatrimestre = 1 and year(alumno.fecha_inicio) = year(current_date)
-        group by profesor.id_departamento
-    ) as horas on th.id_departamento = horas.id_departamento
-    set th.total_horas = horas.total_horas, th.semestre = 1, th.año = year(current_date);
-    update TotalHoras th
-    join (
-        select profesor.id_departamento, sum(alumno.creditos * 25) as total_horas
-        from asignatura
-        join profesor on alumno.id_profesor = profesor.id_profesor
-        where alumno.cuatrimestre = 2 and year(alumno.fecha_inicio) = year(current_date)
-        group by profesor.id_departamento
-    ) as horas on thd.id_departamento = horas.id_departamento
-    set thd.total_horas = horas.total_horas, thd.semestre = 2, thd.año = year(current_date);
-end //
-delimiter ;
-
 -- 3. AlertaAsignaturaNoCursadaAnual: Envía una alerta cuando una asignatura no ha sido cursada en el último año.
-delimiter //
-create event AlertaAsignatura
-on schedule every 1 year
-starts '2025-01-01 00:00:00' do
-begin
-    insert into Alertas (mensaje)
-    select concat('La asignatura "', alumno.nombre, '" no ha sido cursada en el último año.')
-    from asignatura
-    left join alumno_se_matricula_asignatura on alumno.id_asignatura = alumno_se_matricula_asignatura.id_asignatura
-    left join curso_escolar on alumno_se_matricula_asignatura.id_curso_escolar = curso_escolar.id_curso_escolar
-    where curso_escolar.anyo_inicio = (select max(anyo_inicio) from curso_escolar)
-    and alumno_se_matricula_asignatura.id_asignatura is null;
-end //
-delimiter ;
 
 -- 4. LimpiarAuditoriaCadaSemestre: Borra los registros antiguos de auditoría al final de cada semestre.
 create event eliminar_evento1 on schedule 
@@ -71,4 +58,3 @@ where cuatrimestre < month(1) - interval 1 month;
 create event nuevo_profesor1 on schedule
 every 10 hour starts '2024-12-02  10:00' ends '2024-12-02  10:01' 
 do update nuevo_profesor set value = value + 1 where id_profesor = 1;
-
